f188bb111327f2312fb33c9f84c9eb145b227805
Joao Rodrigues <jonezineworks@gmail.com>
2024-02-19 21:18:09 +0000

    Update address to get data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
+import gsap from 'gsap';
 export default {
     // Properties returned from data() become reactive state
     // and will be exposed on `this`.
     data() {
         return {
             houseData: {Power: 0, Today: 0},
+            houseDataPower: 0,
             solarData: {Power: 0, Today: 0},
+            solarDataPower: 0,
             weatherData: {weather: [{description: "", icon: null}], main: {temp: 0, humidity: 0}, altIcon: ""},
         }
     },
@@ -16,7 +19,7 @@ export default {
         connectWebsocket() {
             let that = this;
             console.log("Starting connection to WebSocket Server");
-            this.connection = new WebSocket("ws://emonpi:1880/");
+            this.connection = new WebSocket("ws://192.168.2.132:1880/");
 
             this.connection.onmessage = function (event) {
                 let wsData = JSON.parse(event.data);
@@ -37,15 +40,23 @@ export default {
             };
         }
     },
-
+    watch: {
+        houseData(data) {
+            gsap.to(this, { duration: 6, houseDataPower: Number(data.Power) || 0 })
+        },
+        solarData(data) {
+            gsap.to(this, { duration: 6, solarDataPower: Number(data.Power) || 0 })
+        }
+    },
     // Lifecycle hooks are called at different stages
     // of a component's lifecycle.
     // This function will be called when the component is mounted.
     mounted() {
         this.connectWebsocket();
+        fetch('http://192.168.2.132:1880/weather');
     }
 }
-</script>
+</script>i
 
 <template>
     <div class="vh-100 vh-100 m-0 text-light bg-dark d-flex justify-content-center align-content-center">
@@ -65,14 +76,14 @@ export default {
         </div>
         <div class="m-auto">
             <h2 class="text-info">Consumo Casa </h2>
-            <h1>{{ houseData.Power }} <span class="text-info small">W</span></h1>
+            <h1>{{ houseDataPower.toFixed(0) }} <span class="text-info small">W</span></h1>
             <p class="mb-0 text-secondary">Total Hoje</p>
             <h2>{{ houseData.Today }} <span class="text-info small">KWh</span></h2>
             ~{{ (houseData.Today * 0.14).toFixed(2) }} €
         </div>
         <div class="m-auto">
             <h2 class="text-info">Energia Solar </h2>
-            <h1>{{ solarData.Power }} <span class="text-info small">W</span></h1>
+            <h1>{{ solarDataPower.toFixed(0) }} <span class="text-info small">W</span></h1>
             <p class="mb-0 text-secondary">Total Hoje</p>
             <h2>{{ solarData.Today }} <span class="text-info small">KWh</span></h2>
             ~{{ (solarData.Today * 0.14).toFixed(2) }} €
